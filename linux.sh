99b12d8398a0d3e598fbcfd608fc7fefd41f897c
Kiva <kiva515@foxmail.com>
2016-03-26 13:03:50 +0800

    Update linux.sh
    Fix shebang

--- linux.sh
+++ linux.sh
@@ -1,3 +1,5 @@
+#!/system/bin/bash
+
 # Copyright (C) 2015  Kiva
 
 # This program is free software: you can redistribute it and/or modify
@@ -12,8 +14,6 @@
 
 #You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/system/bin/bash
 
 ###########################
 #
